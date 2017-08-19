630292aa92e7d8a599840e4d03410d51100bcced
Frank Potthast Rutz <frank.potthast@gmail.com>
2017-08-19 11:18:53 +0200

    Lösung eingeben, von David schriftlich gelöst.

--- sudoku.go
+++ sudoku.go
@@ -69,17 +69,17 @@ func printSudo(arr [][]int) {
 // Versuche, das Sudoku-Rätsel zu lösen.
 // Momentan passiert hier noch gar nichts.
 func solveSudo(arr [][]int) {
-	arr[0] = []int{6, 0, 0, 4, 0, 0, 0, 0, 5}
-	arr[1] = []int{2, 9, 4, 6, 0, 0, 7, 0, 0}
-	arr[2] = []int{0, 0, 0, 0, 0, 0, 4, 6, 1}
+	arr[0] = []int{6, 1, 7, 4, 3, 8, 2, 9, 5}
+	arr[1] = []int{2, 9, 4, 6, 1, 5, 7, 8, 3}
+	arr[2] = []int{3, 5, 8, 9, 7, 2, 4, 6, 1}
 
-	arr[3] = []int{0, 2, 3, 5, 0, 0, 0, 7, 4}
-	arr[4] = []int{0, 0, 0, 0, 9, 0, 0, 0, 0}
-	arr[5] = []int{8, 7, 0, 0, 0, 6, 5, 3, 0}
+	arr[3] = []int{9, 2, 3, 5, 8, 1, 6, 7, 4}
+	arr[4] = []int{5, 4, 6, 7, 9, 3, 8, 1, 2}
+	arr[5] = []int{8, 7, 1, 2, 4, 6, 5, 3, 9}
 
-	arr[6] = []int{7, 6, 9, 0, 0, 0, 0, 0, 0}
-	arr[7] = []int{0, 0, 5, 0, 0, 9, 3, 2, 7}
-	arr[8] = []int{1, 0, 0, 0, 0, 7, 0, 0, 6}
+	arr[6] = []int{7, 6, 9, 3, 2, 4, 1, 5, 8}
+	arr[7] = []int{4, 8, 5, 1, 6, 9, 3, 2, 7}
+	arr[8] = []int{1, 3, 2, 8, 5, 7, 9, 4, 6}
 }
 
 //Testet, ob der Sudoku-array OK ist.
